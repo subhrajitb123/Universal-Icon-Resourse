106c6faaaa636e4bc900bcf48f01d0ff90c28d50
subhrajitb123 <subhrajitb@letsupgrade.in>
2023-01-26 16:17:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> helath issue is the prime issue of the socity if the health issue continous then no body cares about the ehelayh';k'j</p>
-      </header>
+      <p>Hello world you are welcome to this world</p>
     </div>
   );
 }
