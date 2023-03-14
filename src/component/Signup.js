e81d68dfe8f16bedb7c5ea1b4a1b61c4d4aa276e
subhrajitb123 <123314649+subhrajitb123@users.noreply.github.com>
2023-03-14 16:09:18 +0530

    Update Signup.js

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -115,7 +115,6 @@ const SignUp = () => {
 							<div className="flex items-center justify-between mb-4">
 								<button
 									className=" w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-									type="submit"
 									onClick={handlesubmit1}
 								>
 									Google
